249bd11cae920935e35c7b8df9daa967ec872ca1
Dominik <rakdominikrak@gmail.com>
2019-04-26 09:36:22 +0200

    możliwość wykonania testów kilka razy i oddzielenie wykonania od logiki

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -3,12 +3,19 @@ package edu.iis.mto.multithread;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import org.junit.Rule;
 import org.junit.Test;
+
+import edu.iis.mto.multithread.RepeatRule.Repeat;
 
 public class RadarTest {
 
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
+
     @Test
-    public void launchPatriotOnceWhenNoticesAScudMissle() {
+    @Repeat(times = 1000)
+    public void launchPatriotOnceWhenNoticesAScudMissle() throws InterruptedException {
         PatriotBattery batteryMock = mock(PatriotBattery.class);
         Radar radar = new Radar(batteryMock);
         radar.notice(new Scud());
