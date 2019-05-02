7d1f03e961d571a664bab8bb9af9966f175c7d16
Zawiasa.Jakub <zawiasa.jakub@gmail.com>
2019-05-02 13:16:12 +0200

    BetterRadar test

--- mutlithreading/src/main/java/edu/iis/mto/multithread/TasksInOneThreadExecutor.java
+++ mutlithreading/src/main/java/edu/iis/mto/multithread/TasksInOneThreadExecutor.java
@@ -2,7 +2,7 @@ package edu.iis.mto.multithread;
 
 import java.util.concurrent.Executor;
 
-public class TaskInOneThreadExecutor implements Executor {
+public class TasksInOneThreadExecutor implements Executor {
 
     @Override
     public void execute(Runnable command) {
