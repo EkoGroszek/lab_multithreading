45f4bb808648023e10e3166673a362f2c8fd98c8
Marax97 <44068049+Marax97@users.noreply.github.com>
2019-05-08 18:11:54 +0200

    radar is tested 100 times

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -3,11 +3,18 @@ package edu.iis.mto.multithread;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import org.junit.Rule;
 import org.junit.Test;
+
+import edu.iis.mto.multithread.RepeatRule.Repeat;
 
 public class RadarTest {
 
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
+
     @Test
+    @Repeat(times = 100)
     public void launchPatriotOnceWhenNoticesAScudMissle() {
         PatriotBattery batteryMock = mock(PatriotBattery.class);
         Radar radar = new Radar(batteryMock);
