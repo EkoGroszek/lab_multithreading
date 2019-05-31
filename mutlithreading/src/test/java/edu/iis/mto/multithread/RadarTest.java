f334fb2d8edfc477d758bad9cebfb5fdff57adc0
Kamil Żarski <209526@edu.p.lodz.pl>
2019-06-01 00:13:01 +0200

    New test for checking 3 Patriot launched, in new Thread

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,27 +1,35 @@
 package edu.iis.mto.multithread;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.mockito.Mockito.*;
 
 public class RadarTest {
 
     private NewExecutor NEWExecutor;
+    private NewExecutor THISExecutor;
     private PatriotBattery batteryMock;
 
     @Before
     public void init() {
         NEWExecutor = new NewExecutor(JobType.NEW);
+        THISExecutor = new NewExecutor(JobType.THIS);
         batteryMock = mock(PatriotBattery.class);
     }
 
     @Test
     public void launchPatriotOnceWhenNoticesAScudMissle() {
-        NewRadar radar = new NewRadar(batteryMock, NEWExecutor, 1);
+        NewRadar radar = new NewRadar(batteryMock, THISExecutor, 1);
         radar.notice(new Scud());
         verify(batteryMock).launchPatriot();
+    }
+
+    @Test
+    public void launchPatriot3TimeInNewThread(){
+        NewRadar radar = new NewRadar(batteryMock, NEWExecutor, 3);
+        radar.notice(new Scud());
+        verify(batteryMock,times(3)).launchPatriot();
     }
 
 }
