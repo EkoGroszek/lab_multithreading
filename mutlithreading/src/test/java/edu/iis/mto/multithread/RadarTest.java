fef6df7ae9f444749980e9c88f18bc498ae818bb
Kamil Żarski <209526@edu.p.lodz.pl>
2019-06-01 00:05:04 +0200

    repair test for checked radar notice

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -3,14 +3,23 @@ package edu.iis.mto.multithread;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class RadarTest {
 
+    private NewExecutor NEWExecutor;
+    private PatriotBattery batteryMock;
+
+    @Before
+    public void init() {
+        NEWExecutor = new NewExecutor(JobType.NEW);
+        batteryMock = mock(PatriotBattery.class);
+    }
+
     @Test
     public void launchPatriotOnceWhenNoticesAScudMissle() {
-        PatriotBattery batteryMock = mock(PatriotBattery.class);
-        Radar radar = new Radar(batteryMock);
+        NewRadar radar = new NewRadar(batteryMock, NEWExecutor, 1);
         radar.notice(new Scud());
         verify(batteryMock).launchPatriot();
     }
