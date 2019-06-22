9718bafc11058957680dd77d50627a3974c9297f
Caroolinnee <karolinab341@gmail.com>
2019-06-22 23:23:19 +0200

    try run test with 10 times - red

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,18 +1,26 @@
 package edu.iis.mto.multithread;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import org.junit.Rule;
 import org.junit.Test;
 
-public class RadarTest {
+public class RadarTest
+    {
+
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
 
     @Test
-    public void launchPatriotOnceWhenNoticesAScudMissle() {
+    @RepeatRule.Repeat( times = 10 )
+    public void launchPatriotOnceWhenNoticesAScudMissle()
+        {
         PatriotBattery batteryMock = mock(PatriotBattery.class);
         Radar radar = new Radar(batteryMock);
         radar.notice(new Scud());
-        verify(batteryMock).launchPatriot();
-    }
+        verify(batteryMock,times(1)).launchPatriot();
+        }
 
-}
+    }
