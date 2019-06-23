947812c82855b349c05998774d5c1467e528bbb1
Bartłomiej Dettlaff <bartlomiejdettlaff@interia.pl>
2019-06-23 14:04:30 +0200

    Refactor name of first test and add Repeat Rule

--- mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -1,14 +1,18 @@
 package edu.iis.mto.multithread;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class BetterRadarTest {
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
 
     @Test
-    public void launchPatriotOnceWhenNoticesAScudMissle() {
+    @RepeatRule.Repeat(times = 1000)
+    public void launchPatriotOnceWhenNoticesAScudMissile() {
         PatriotBattery batteryMock = mock(PatriotBattery.class);
         Radar radar = new Radar(batteryMock);
         radar.notice(new Scud());
