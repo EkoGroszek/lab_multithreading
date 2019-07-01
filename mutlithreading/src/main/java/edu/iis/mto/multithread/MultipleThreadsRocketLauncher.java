6299881b4252b816e5b3827dee99b9cdfebb57d0
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-07-01 23:52:08 +0200

    MultipleThreadsRocketLauncher class added

---
+++ mutlithreading/src/main/java/edu/iis/mto/multithread/MultipleThreadsRocketLauncher.java
@@ -0,0 +1,26 @@
+package edu.iis.mto.multithread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MultipleThreadsRocketLauncher implements RocketLauncher {
+
+    private ExecutorService executorService;
+
+    public MultipleThreadsRocketLauncher(ExecutorService executorService) {
+        this.executorService = executorService;
+    }
+
+    @Override public void launchRockets(PatriotBattery patriotBattery, int numberOfRockets) {
+
+        if (executorService == null) {
+            executorService = Executors.newFixedThreadPool(numberOfRockets);
+        }
+
+        for (int i = 0; i < numberOfRockets; i++) {
+            executorService.execute(patriotBattery::launchPatriot);
+        }
+        executorService.shutdown();
+
+    }
+}
